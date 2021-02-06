b7eb71a3670301581a7ac7525f5b2f991cd5bcb2
unknown <g00398265@gmit.ie>
2021-02-06 17:58:57 +0000

    testTypes

---
+++ week3/testTypes.py
@@ -0,0 +1,14 @@
+#This program evalutes the types of variables
+#list (look at w3schools.com)
+
+i=2
+fl=2.4
+ts=True
+st="Hello"
+lots=[]
+
+print('variable {} is of type: {} and its value is: {}' .format('i',type(i),i))
+print('variable {} is of type: {} and its value is: {}' .format('fl',type(fl),fl))
+print('variable {} is of type: {} and its value is: {}' .format('ts',type(ts),ts))
+print('variable {} is of type: {} and its value is: {}' .format('st',type(st),st))
+print('variable {} is of type: {} and its value is: {}' .format('lots',type(lots),lots))
