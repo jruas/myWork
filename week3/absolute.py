8f348b4326f3e1fc5b086000ebfc45e962088705
unknown <g00398265@gmit.ie>
2021-02-06 18:51:10 +0000

    absolute.py

---
+++ week3/absolute.py
@@ -0,0 +1,10 @@
+#This program takes in a number and give its absolute value
+#Author: Joana Ruas
+
+#Taking a number
+a=float(input('Please insert a number'))
+
+#Turning the number into absolute
+b=abs(a)
+
+print('The absolute value of {} is {}'.format(a,b))
---
+++ week3/round.py
@@ -0,0 +1,6 @@
+#The program take in a float and outputs an int (rounded up or down)
+
+x=float(input('Please insert a number: '))
+a=int(round(x))
+
+print(' {} rounded is {}'.format(x,a))
