f003524c8c8600eb39ae04bf48e451bb22ada011
unknown <g00398265@gmit.ie>
2021-02-06 18:12:22 +0000

    sub.py

---
+++ week3/sub.py
@@ -0,0 +1,7 @@
+# This program reads in two numbers and subracts the first
+# Author: Joana Ruas
+
+num1=input('Please insert a number')
+num2=input('Please insert a second number')
+answer=(int(num1)-int(num2))
+print('{} minus {} is {}'. format(num1, num2, answer))
